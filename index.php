397254ee4894ded925a980d24b9d98773ca22339
Nick <nick@nickplain.com>
2012-12-04 10:51:06 -0500

    Retrying update

--- index.php
+++ index.php
@@ -5,9 +5,12 @@ Working with Github
 */
 
 ?>
+<<<<<<< HEAD
 
 <html>
 <body>
 Well well well looky here, I'm testing out my new site using the Orion browser-based IDE and hooking into Github
 </body>
 </html>
+=======
+>>>>>>> 1ff9d7f19b8cdc90d5dcc703d148a96ff41b7939
