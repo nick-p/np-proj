9a2a5bfbf8d6111a0742f43aba7baef7edfa3c8a
Nick <nick@nickplain.com>
2012-12-04 10:47:37 -0500

    Update index.php (basic test)

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/*index file for np-proj
+Orion test
+Working with Github
+*/
+
+?>
+
+<html>
+<body>
+Well well well looky here, I'm testing out my new site using the Orion browser-based IDE and hooking into Github
+</body>
+</html>
